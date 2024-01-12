9abe8a5c83a2135eeb55a8c664fe7bc74641947e
bilbobaggins <bill.weaver@hotmail.co.uk>
2024-01-12 08:57:59 +0000

    analyse health data

---
+++ src/WebSite/Analytics.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Analytics.aspx.cs" Inherits="Analytics" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <p>
+                Data file:
+                <asp:TextBox ID="uiDataFile"  CssClass="form-rounded w-72" runat="server" />
+            </p>
+        </div>
+        <div>
+            Enter search text:
+            <asp:TextBox ID="uiSearchText" CssClass="form-rounded w-72" runat="server" />
+            <asp:Button ID="btnSearch" runat="server" Text="Search" OnClick="GetSearchResults" CssClass="nav-link" />
+        </div>
+        <div>
+            <p>
+                <asp:Literal ID="uiLtlOutput" runat="server" />
+            </p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ src/WebSite/Analytics.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Analytics : System.Web.UI.Page
+{
+    protected string filePath;
+    protected string currentDate;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // Specify the path to the text file
+        filePath = "c:/Temp/testdata.txt";
+    }
+    protected void GetSearchResults(object sender, EventArgs e)
+    {
+        try
+        {
+            if (!string.IsNullOrEmpty(uiDataFile.Text))
+            {
+                filePath = uiDataFile.Text;
+            }
+
+            // Read all lines from the file
+            string[] lines = File.ReadAllLines(filePath);
+
+            uiLtlOutput.Text = string.Empty;
+
+            foreach (string line in lines)
+            {
+                if (line.StartsWith("Date"))
+                {
+                    currentDate = line;
+                }
+                else
+                {
+                    if (line.ToLower().Contains(uiSearchText.Text.ToLower()))
+                    {
+                        uiLtlOutput.Text += "<b>" + currentDate + "</b>";
+                        uiLtlOutput.Text += line + "</br>";
+                    }
+
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Response.Write($"An error occurred while reading the file: {ex.Message}");
+        }
+    }
+}
