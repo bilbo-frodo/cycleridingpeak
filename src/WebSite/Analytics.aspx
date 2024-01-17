da9a203d67d00dd881b4ac842524a2f40380db3c
bilbobaggins <bill.weaver@hotmail.co.uk>
2024-01-17 16:15:05 +0000

    general tidying up

--- src/WebSite/Analytics.aspx
+++ src/WebSite/Analytics.aspx
@@ -5,24 +5,40 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
+    <style>
+        .w-200{ width: 200px;}
+        .w-300{ width: 300px; }
+        .w-108 {width:108px; text-align:right;}
+        .w-96 {width:96px; text-align:right;}
+        .w-84 {width:84px; text-align:right;}
+        .w-72 {width:72px; text-align:right;}
+        .w-64 {width:64px; text-align:right;}
+        .w-56 {width:56px; text-align:right;}
+        .w-48 {width:48px; text-align:right;}
+        .w-40 {width:40px; text-align:right;}
+        .w-0 {width:0; display:none;}
+        .form-rounded {border-radius:4px; border-width:1px; padding: 2px;}
+    </style>
 </head>
 <body>
     <form id="form1" runat="server">
         <div>
             <p>
                 Data file:
-                <asp:TextBox ID="uiDataFile"  CssClass="form-rounded w-72" runat="server" />
+                <asp:TextBox ID="uiDataFile"  CssClass="form-rounded w-300" runat="server" />
             </p>
         </div>
         <div>
             Enter search text:
-            <asp:TextBox ID="uiSearchText" CssClass="form-rounded w-72" runat="server" />
+            <asp:TextBox ID="uiSearchText" CssClass="form-rounded w-200" runat="server" />
             <asp:Button ID="btnSearch" runat="server" Text="Search" OnClick="GetSearchResults" CssClass="nav-link" />
         </div>
         <div>
-            <p>
+            <br />
+            <table>
+                <tr><td class="w-200">Date</td><td></td></tr>                
                 <asp:Literal ID="uiLtlOutput" runat="server" />
-            </p>
+            </table>
         </div>
     </form>
 </body>
--- src/WebSite/Analytics.aspx.cs
+++ src/WebSite/Analytics.aspx.cs
@@ -9,17 +9,28 @@ using System.Web.UI.WebControls;
 public partial class Analytics : System.Web.UI.Page
 {
     protected string filePath;
-    protected string currentDate;
+    protected string strCurrentDate, nextLineDate=string.Empty;
+    protected string searchHit=string.Empty;
+    protected bool dateFound = false;
 
     protected void Page_Load(object sender, EventArgs e)
     {
         // Specify the path to the text file
-        filePath = "c:/Temp/testdata.txt";
+        filePath = "C:/BillShortcuts/Health/Health Data/Data2023.txt";
+        if (!IsPostBack)
+        {
+            uiDataFile.Text = filePath;
+        }
     }
     protected void GetSearchResults(object sender, EventArgs e)
     {
         try
         {
+            if (string.IsNullOrEmpty(uiSearchText.Text))
+            {
+                return; // have no search text
+            }
+
             if (!string.IsNullOrEmpty(uiDataFile.Text))
             {
                 filePath = uiDataFile.Text;
@@ -34,14 +45,30 @@ public partial class Analytics : System.Web.UI.Page
             {
                 if (line.StartsWith("Date"))
                 {
-                    currentDate = line;
+                    strCurrentDate = line.Replace("�", string.Empty);
+                    dateFound = true;
                 }
                 else
                 {
                     if (line.ToLower().Contains(uiSearchText.Text.ToLower()))
                     {
-                        uiLtlOutput.Text += "<b>" + currentDate + "</b>";
-                        uiLtlOutput.Text += line + "</br>";
+                        uiLtlOutput.Text += "<tr>";
+                        if (dateFound)
+                        {
+                            uiLtlOutput.Text += "<td style='vertical-align:top'>" + GetDateFrom(strCurrentDate) + "</td>";  //remove the 'Date: ' string
+                            dateFound = false;
+                        } else
+                        {
+                            uiLtlOutput.Text += "<td></td>";
+                        }
+                        
+                        searchHit = line.Replace("�", "'");  // file.readalllines changes quotes to '?' so change it back
+
+                        // highlight the search term
+                        searchHit = searchHit.Replace(uiSearchText.Text, "<b>" + uiSearchText.Text + "</b>");
+
+                        uiLtlOutput.Text += "<td>" + searchHit + " </td>";    
+                        uiLtlOutput.Text += "</tr>";
                     }
 
                 }
@@ -51,5 +78,16 @@ public partial class Analytics : System.Web.UI.Page
         {
             Response.Write($"An error occurred while reading the file: {ex.Message}");
         }
+    }
+
+    /// <summary>
+    /// remove 'Date: ' from the input line
+    /// </summary>
+    /// <param name="dateLine"></param>
+    /// <returns></returns>
+    protected string GetDateFrom(string dateLine)
+    {
+        int line = dateLine.IndexOf("Date:");
+        return dateLine.Substring(line + 5).TrimStart(' ');
     }
 }
